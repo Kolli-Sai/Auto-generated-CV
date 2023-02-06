ea62f60f832fc22e350fbcb7f57ac7b6071e48d2
Kolli-Sai <116951565+Kolli-Sai@users.noreply.github.com>
2023-02-06 14:57:55 +0530

    handle print updated

--- app.js
+++ app.js
@@ -1,119 +1,115 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
+// import all the fields from the form.
+// eg.
+// const name = document.getElementById("name");
+
+// function handleSubmit(e) {
+//   e.preventDefault();
 function handleSubmit(e) {
   e.preventDefault();
-  // if (document.querySelector("input").value == "") {
-  //   alert("please fill all required fields");
-  // }
-  // name
-  let name = document.getElementById("Name").value;
+
+  var name = document.getElementById("name").value;
   document.getElementById("display-name").innerHTML = name;
-  document.getElementById("Name").value = "";
+  document.getElementById("name").value = "";
+  //---------------email
 
-  // email
-  let email = document.getElementById("Email").value;
-  document.getElementById("display-email").innerHTML = email + " |";
+  var name = document.getElementById("Email").value;
+  document.getElementById("display-Email").innerHTML = name;
   document.getElementById("Email").value = "";
-  if (document.getElementById("Email").value == null) {
-    document.getElementById("display-email").innerHTML = "";
-  }
-  // phone
-  let phone = document.getElementById("Phone").value;
-  document.getElementById("display-phone").innerHTML = phone + " |";
+  //-----------------phone
+
+  var name = document.getElementById("Phone").value;
+  document.getElementById("display-Phone").innerHTML = name;
   document.getElementById("Phone").value = "";
-  if (document.getElementById("Phone").value == null) {
-    document.getElementById("display-phone").innerHTML = "";
-  }
+  //---------------------github
 
-  // github
-  let github = document.getElementById("Github").value;
-  document.getElementById("display-github").innerHTML = github + " |";
+  var name = document.getElementById("Github").value;
+  document.getElementById("display-Github").innerHTML = name;
   document.getElementById("Github").value = "";
-  if (document.getElementById("Github").value == null) {
-    document.getElementById("display-github").innerHTML = "";
-  }
-  // linkedin
-  let linkedin = document.getElementById("Linkedin").value;
-  document.getElementById("display-linkedin").innerHTML = linkedin;
+  //------------lik
+  var name = document.getElementById("Linkedin").value;
+  document.getElementById("display-Linkedin").innerHTML = name;
   document.getElementById("Linkedin").value = "";
-
-  // education
-  // degree
-  let degree = document.getElementById("Degree").value;
-  document.getElementById("display-degree").innerHTML = degree;
+  //----------------------------------------------edu
+  var name = document.getElementById("Degree").value;
+  document.getElementById("display-Degree").innerHTML = name;
   document.getElementById("Degree").value = "";
-  // college
-  let college = document.getElementById("College").value;
-  document.getElementById("display-college").innerHTML = college;
+
+  var name = document.getElementById("College").value;
+  document.getElementById("display-College").innerHTML = name;
   document.getElementById("College").value = "";
-  // startingyear
-  let startingyear = document.getElementById("StartingYear").value;
-  document.getElementById("display-startingyear").innerHTML = startingyear;
-  document.getElementById("StartingYear").value = "";
-  // passingyear
-  let passingyear = document.getElementById("PassingYear").value;
-  document.getElementById("display-passingyear").innerHTML =
-    " - " + passingyear;
-  document.getElementById("PassingYear").value = "";
-  if (document.getElementById("PassingYear").value == null) {
-    document.getElementById("display-passingyear").innerHTML = "";
-  }
-  // cgpa
-  let cgpa = document.getElementById("CGPA").value;
-  document.getElementById("display-cgpa").innerHTML = "CGPA : " + cgpa;
+
+  var name = document.getElementById("Starting Year").value;
+  document.getElementById("display-Starting Year").innerHTML = name;
+  document.getElementById("Starting Year").value = "";
+
+  var name = document.getElementById("Passing Year").value;
+  document.getElementById("display-Passing Year").innerHTML = name;
+  document.getElementById("Passing Year").value = "";
+
+  var name = document.getElementById("CGPA").value;
+  document.getElementById("display-CGPA").innerHTML = name;
   document.getElementById("CGPA").value = "";
-  if (document.getElementById("CGPA").value == "") {
-    document.getElementById("display-cgpa").innerHTML = "";
-  }
 
-  // skills
-  let skills = document.getElementById("Skills").value;
-  document.getElementById("display-skills").innerHTML = skills;
+  //-------------------skills
+  var name = document.getElementById("Skills").value;
+  document.getElementById("display-Skills").innerHTML = name;
   document.getElementById("Skills").value = "";
-  // project
-  let project = document.getElementById("Project").value;
-  document.getElementById("display-project").innerHTML = project;
-  document.getElementById("Project").value = "";
-  // description;
-  let description = document.getElementById("Description").value;
-  document.getElementById("display-description").innerHTML = description;
-  document.getElementById("Description").value = "";
-  // link
-  let link = document.getElementById("Link").value;
-  document.getElementById("display-link").innerHTML = "Link - " + link;
-  document.getElementById("Link").value = "";
-  if (document.getElementById("Link").value == null) {
-    document.getElementById("display-link").innerHTML = "";
-  }
 
-  // company
-  let company = document.getElementById("Company").value;
-  document.getElementById("display-company").innerHTML = company;
+  //----------------------------project
+  var name = document.getElementById("Project").value;
+  document.getElementById("display-Project").innerHTML = name;
+  document.getElementById("Project").value = "";
+
+  var name = document.getElementById("Description").value;
+  document.getElementById("display-Description").innerHTML = name;
+  document.getElementById("Description").value = "";
+
+  var name = document.getElementById("Link").value;
+  document.getElementById("display-Link").innerHTML = name;
+  document.getElementById("Link").value = "";
+
+  var name = document.getElementById("Company").value;
+  document.getElementById("display-Company").innerHTML = name;
   document.getElementById("Company").value = "";
-  // designation
-  let designation = document.getElementById("Designation").value;
-  document.getElementById("display-designation").innerHTML = designation;
+
+  var name = document.getElementById("Designation").value;
+  document.getElementById("display-Designation").innerHTML = name;
   document.getElementById("Designation").value = "";
-  // duration
-  let duration = document.getElementById("Duration").value;
-  document.getElementById("display-duration").innerHTML = duration;
+
+  var name = document.getElementById("Duration").value;
+  document.getElementById("display-Duration").innerHTML = name;
   document.getElementById("Duration").value = "";
-  // description2
-  let description2 = document.getElementById("Description2").value;
-  document.getElementById("display-description2").innerHTML = description2;
-  document.getElementById("Description2").value = "";
+
+  var name = document.getElementById("description").value;
+  document.getElementById("display-description").innerHTML = name;
+  document.getElementById("description").value = "";
 }
+
+//   here get the values from the form and set it to the resume
+// eg.
+//   const name_resume = document.getElementById("name_resume");
+//   name_resume.innerHTML = name.value;
+//   similarly for all the other fields
 
 function handleLeft() {
   const left = document.querySelector(".left");
   const right = document.querySelector(".right");
   const print = document.querySelector(".print");
+  left.style.display = "none";
+  right.style.width = "100%";
+  print.style.display = "block";
   // write more codes here
 }
 function handlePrint() {
   const right = document.querySelector(".right");
   const left = document.querySelector(".left");
   const print = document.querySelector(".print");
+  window.print();
+  left.style.display = "block";
+  right.style.width = "50%";
+  print.style.display = "none";
   // write more codes here
 }
